be2c6176092b5f3eb344c56f61eb93b815d65444
baekminwoo <mwb970419@gmail.com>
2024-10-17 22:08:18 +0900

    1017

--- 10-17/1260. 백준 실버2 DFS와 BFS.py
+++ 10-17/1260. 백준 실버2 DFS와 BFS.py
@@ -7,6 +7,7 @@ n, m, start = map(int, input().split())
 lst = [[] for _ in range(n + 1)]
 v = [0 for _ in range(n + 1)]
 
+# 양방향으로 방문 가능하니까 둘다 추가
 for _ in range(m):
     a, b = map(int, input().split())
     lst[a].append(b)
@@ -16,21 +17,28 @@ for _ in range(m):
 for i in lst:
     i.sort()
 
-v[start] = 1
+
 def dfs(num):
     print(num, end=' ')
     for i in lst[num]:
         if not v[i]:
             v[i] = 1
             dfs(i)
-
+            
+# 재귀로 해결하기 넣기 전에 방문 처리 해주고 dfs 시작
+v[start] = 1
 dfs(start)
 print()
+
+
+#여기서부터는 bfs시작
 
 v = [0 for _ in range(n + 1)]
 q = deque()
 q.append(start)
 v[start] = 1
+
+# 잘보면 dfs랑 bfs 생긴게 비슷하다
 while q:
     num = q.popleft()
     print(num, end=' ')
@@ -38,5 +46,6 @@ while q:
         if not v[j]:
             v[j]=1
             q.append(j)
+            
 print()
 
